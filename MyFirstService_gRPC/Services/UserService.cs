6cfcf5be0f2cc44902e981da3626eb95435d6d17
Mladen Perišić <mladen.perisic@comminus.hr>
2021-05-18 21:17:45 +0200

    i am commit

---
+++ MyFirstService_gRPC/Services/UserService.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstService_gRPC
+{
+    public class UserService : User.UserBase
+    {
+        public override Task<UserReply> GetUserById(UserIdRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new UserReply
+            {
+                Name = "Denis",
+                PersonNumber = 1,
+                Surename = "Predala"
+            });
+        }
+    }
+}
